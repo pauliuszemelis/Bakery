9b8f5d7e0f3e441da16d7d870a0fd3c687f92710
Paulius Žemelis <paulius.zemelis@gmail.com>
2017-11-09 19:08:18 +0200

    sukurta prekių pavadinimų lentelė

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Bakery Online</title>
+	<link rel="stylesheet" type="text/css" href="css/app.css">
+</head>
+<body>
+	<table id="prekes" border=1px >
+		
+		<thead align="center">
+			<td>Pavadinimas</td>	
+		</thead>
+		<tbody>
+			<tr>
+				<td>Bandelė "Aguonėlė"</td>
+			</tr>
+			<tr>
+				<td>Duona "Balta"</td>
+			</tr>
+			<tr>
+				<td>Duona "Juoda"</td>
+			</tr>
+			<tr>
+				<td>Duona "Ruginė"</td>
+			</tr>
+			<tr>
+				<td>Bandelė "Persikas"</td>
+			</tr>
+			
+		</tbody>
+
+
+
+	</table>
+
+
+</body>
+</html>
