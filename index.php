f4b654e4d592ac8cbdcf6c51d18d54031412bcbd
Paulius Žemelis <paulius.zemelis@gmail.com>
2017-11-09 19:37:17 +0200

    Pridėta antra diena

--- index.php
+++ index.php
@@ -6,13 +6,18 @@
 </head>
 <body>
 	<table id="prekes" border=1px >
-		
 		<thead align="center">
 			<tr>
 				<th rowspan="2">Pavadinimas</th>
 				<th colspan="5">2017-11-09</th>
+				<th colspan="5">2017-11-10</th>
 			</tr>	
 			<tr>
+				<th>VL</th> <!-- Vakar dienos likutis -->
+				<th>PG</th>	<!-- Pagaminta -->
+				<th>PR</th>	<!-- Parduota -->	
+				<th>SG</th>	<!-- Sugadinta -->	
+				<th>GL</th>	<!-- Galutinis likutis -->
 				<th>VL</th> <!-- Vakar dienos likutis -->
 				<th>PG</th>	<!-- Pagaminta -->
 				<th>PR</th>	<!-- Parduota -->	
@@ -28,48 +33,65 @@
 				<td>20</td>
 				<td>5</td>
 				<td>5</td>
-				
-
-			</tr>
-			<tr>
-				<td>Duona "Balta"</td>
 				<td>5</td>
 				<td>25</td>
 				<td>20</td>
 				<td>5</td>
+				<td>5</td>
+			</tr>
+			<tr>
+				<td>Duona "Balta"</td>
+				<td>3</td>
+				<td>27</td>
+				<td>15</td>
+				<td>10</td>
+				<td>5</td>
+				<td>3</td>
+				<td>27</td>
+				<td>15</td>
+				<td>10</td>
 				<td>5</td>
 			</tr>
 			<tr>
 				<td>Duona "Juoda"</td>
-				<td>5</td>
-				<td>25</td>
-				<td>20</td>
-				<td>5</td>
-				<td>5</td>
+				<td>4</td>
+				<td>24</td>
+				<td>27</td>
+				<td>0</td>
+				<td>1</td>
+				<td>4</td>
+				<td>24</td>
+				<td>27</td>
+				<td>0</td>
+				<td>1</td>
 			</tr>
 			<tr>
 				<td>Duona "Ruginė"</td>
-				<td>5</td>
+				<td>6</td>
+				<td>24</td>
 				<td>25</td>
-				<td>20</td>
-				<td>5</td>
-				<td>5</td>
+				<td>2</td>
+				<td>3</td>
+				<td>6</td>
+				<td>24</td>
+				<td>25</td>
+				<td>2</td>
+				<td>3</td>
 			</tr>
 			<tr>
 				<td>Bandelė "Persikas"</td>
-				<td>5</td>
+				<td>7</td>
 				<td>25</td>
 				<td>20</td>
 				<td>5</td>
+				<td>7</td>
+				<td>7</td>
+				<td>25</td>
+				<td>20</td>
 				<td>5</td>
+				<td>7</td>
 			</tr>
-			
 		</tbody>
-
-
-
 	</table>
-
-
 </body>
 </html>
