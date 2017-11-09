94e9dd7dece3e9d4e7e9da3d3369764be90d0ba8
Paulius Žemelis <paulius.zemelis@gmail.com>
2017-11-09 19:29:52 +0200

    sudaryta dienos lentelė

--- index.php
+++ index.php
@@ -8,23 +8,60 @@
 	<table id="prekes" border=1px >
 		
 		<thead align="center">
-			<td>Pavadinimas</td>	
+			<tr>
+				<th rowspan="2">Pavadinimas</th>
+				<th colspan="5">2017-11-09</th>
+			</tr>	
+			<tr>
+				<th>VL</th> <!-- Vakar dienos likutis -->
+				<th>PG</th>	<!-- Pagaminta -->
+				<th>PR</th>	<!-- Parduota -->	
+				<th>SG</th>	<!-- Sugadinta -->	
+				<th>GL</th>	<!-- Galutinis likutis -->
+			</tr>	
 		</thead>
 		<tbody>
 			<tr>
 				<td>Bandelė "Aguonėlė"</td>
+				<td>5</td>
+				<td>25</td>
+				<td>20</td>
+				<td>5</td>
+				<td>5</td>
+				
+
 			</tr>
 			<tr>
 				<td>Duona "Balta"</td>
+				<td>5</td>
+				<td>25</td>
+				<td>20</td>
+				<td>5</td>
+				<td>5</td>
 			</tr>
 			<tr>
 				<td>Duona "Juoda"</td>
+				<td>5</td>
+				<td>25</td>
+				<td>20</td>
+				<td>5</td>
+				<td>5</td>
 			</tr>
 			<tr>
 				<td>Duona "Ruginė"</td>
+				<td>5</td>
+				<td>25</td>
+				<td>20</td>
+				<td>5</td>
+				<td>5</td>
 			</tr>
 			<tr>
 				<td>Bandelė "Persikas"</td>
+				<td>5</td>
+				<td>25</td>
+				<td>20</td>
+				<td>5</td>
+				<td>5</td>
 			</tr>
 			
 		</tbody>
