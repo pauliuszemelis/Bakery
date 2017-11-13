b5497ad59e48cbb754d9623c5c435799eee10f14
Paulius Žemelis <paulius.zemelis@gmail.com>
2017-11-13 21:20:28 +0200

    theader'is sukuriamas ciklu

--- app/view/data-table.php
+++ app/view/data-table.php
@@ -1,12 +1,43 @@
 <?php
+$days = $keys = '';
 
+foreach ($data as $key => $value) {
+					$days .= "<th colspan=\"5\">$key</th>";
+					$keys .= "<th>VL</th><th>PG</th><th>PR</th><th>SG</th><th>GL</th>";
+						//echo '<th colspan="5">'.$key.'</th>'; -- antras variantas
+					};
+				?>
+<table>
+		<thead>
+			<tr>
+				<th rowspan="2">Pavadinimas</th>
+				<?php
+					echo $days;
+				?>
+			</tr>
+			<tr>
+				<?php
+					echo $keys;
+				?>
+
+
+		</thead>
+</table>
+
+<?php
+return;
+/*
 var_dump($data);
 
 $html = '';
 
 $html .='<table>';
+$html .='<thead>';
+
+$html .='</thead>';
 $html .='</table>';
 
 echo $html;
 
-//return;
+*/
+?>
