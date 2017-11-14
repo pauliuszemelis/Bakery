f1b772ed4d035b2d0b55475f0a496d50fc512bf4
Paulius Žemelis <paulius.zemelis@gmail.com>
2017-11-14 22:39:36 +0200

    Automatinis duomenų sukėlimas į lentelę... 1 dalis

--- app/view/data-table.php
+++ app/view/data-table.php
@@ -2,53 +2,63 @@
 $days = $keys = '';
 
 foreach ($data as $key => $value) {
-					$days .= "<th colspan=\"5\">$key</th>";
-					$keys .= "<th>VL</th><th>PG</th><th>PR</th><th>SG</th><th>GL</th>";
-						//echo '<th colspan="5">'.$key.'</th>'; -- antras variantas
-					};
-
-
-foreach ($value as $name => $data) {
-	//name 			//data
-	"Aguonėlė" => [5, 25, 20, 5, 5],
-		
-		foreach ($data as $amount) {
-			# code...
-		}}
-
-
-				?>
+    $days .= "<th colspan=\"5\">$key</th>";
+    $keys .= "<th>VL</th><th>PG</th><th>PR</th><th>SG</th><th>GL</th>";
+}
+?>
 <table>
-		<thead>
-			<tr>
-				<th rowspan="2">Pavadinimas</th>
-				<?php
-					echo $days;
-				?>
-			</tr>
-			<tr>
-				<?php
-					echo $keys;
-				?>
+    <thead>
+        <tr>
+            <th rowspan="2">Pavadinimas</th>
+<?php
+echo $days;
+?>
+        </tr>
+        <tr>
+            <?php
+            echo $keys;
 
 
-		</thead>
+            foreach ($data as $key => $value) { 
+                foreach ($value as $name => $pava) {
+                    foreach ($pava as $amount) {
+                     $_amount .= "<td>$amount</td>";   
+                    }
+                }
+            }
+      
+            foreach ($value as $name => $pava) {
+                echo "<tr><td align = \"left\";>$name</td>$_amount</tr>";
+            }
+
+
+            //echo "key : $key<br>";
+            //echo "value : $value<br>";
+           // echo "name : $name<br>";
+           // echo "_name : $_name<br>";
+           // echo "pava : $pava<br/>";
+//echo "amount : $amount<br>";
+//echo "_amount : $_amount<br>";
+            ?>
+        </tr>
+
+    </thead>
 </table>
 
 <?php
 return;
 /*
-var_dump($data);
+  var_dump($data);
 
-$html = '';
+  $html = '';
 
-$html .='<table>';
-$html .='<thead>';
+  $html .='<table>';
+  $html .='<thead>';
 
-$html .='</thead>';
-$html .='</table>';
+  $html .='</thead>';
+  $html .='</table>';
 
-echo $html;
+  echo $html;
 
-*/
+ */
 ?>
