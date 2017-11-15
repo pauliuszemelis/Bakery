bc084720264b3dac60b18aeb294b8158448acadd
Paulius Žemelis <paulius.zemelis@gmail.com>
2017-11-15 13:49:22 +0200

    Pabaigta lentelė su duomenimis.

--- app/view/data-table.php
+++ app/view/data-table.php
@@ -2,53 +2,51 @@
 $days = $keys = '';
 
 foreach ($data as $key => $value) {
-					$days .= "<th colspan=\"5\">$key</th>";
-					$keys .= "<th>VL</th><th>PG</th><th>PR</th><th>SG</th><th>GL</th>";
-						//echo '<th colspan="5">'.$key.'</th>'; -- antras variantas
-					};
-
-
-foreach ($value as $name => $data) {
-	//name 			//data
-	"Aguonėlė" => [5, 25, 20, 5, 5],
-		
-		foreach ($data as $amount) {
-			# code...
-		}}
-
-
-				?>
+    $days .= "<th colspan=\"5\">$key</th>";
+    $keys .= "<th>VL</th><th>PG</th><th>PR</th><th>SG</th><th>GL</th>";
+}
+?>
 <table>
-		<thead>
-			<tr>
-				<th rowspan="2">Pavadinimas</th>
-				<?php
-					echo $days;
-				?>
-			</tr>
-			<tr>
-				<?php
-					echo $keys;
-				?>
+    <thead>
+        <tr>
+            <th rowspan="2">Pavadinimas</th>
+            <?php
+            echo $days;
+            ?>
+        </tr>
+        <tr>
+            <?php
+            echo $keys;
+            ?>
+        </tr>
+    </thead> 
+    <?php
+    $arr = array();
+    
+    foreach ($data as $key => $value) {
+        foreach ($value as $name => $data) {
+            if (!isset($arr[$name])) {
+                $arr[$name] = array();
+            }
+            $arr[$name][] = $data;
+        }
+    }
+    
+
+    foreach ($arr as $key => $value) {
+        echo "<tr>";
+        echo "<td align=\"left\"><b>" . $key . "</b></td>";
+        foreach ($value as $data) {
+            foreach ($data as $amount) {
+                echo "<td>" . $amount . "</td>";
+            }
+        }
+        echo "</tr>";
+    }
+        
+    ?>
 
 
-		</thead>
+
 </table>
 
-<?php
-return;
-/*
-var_dump($data);
-
-$html = '';
-
-$html .='<table>';
-$html .='<thead>';
-
-$html .='</thead>';
-$html .='</table>';
-
-echo $html;
-
-*/
-?>
