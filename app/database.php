199a6e7072b66897d403da1b7c134f4d9ec667f3
Paulius Žemelis <paulius.zemelis@gmail.com>
2017-11-27 20:30:08 +0200

    print_r products

--- app/database.php
+++ app/database.php
@@ -1,33 +1,30 @@
 <?php
 
+
+
+function db_connect () {
+
 $servername = '127.0.0.1';
 $username = 'root';
 $password = '';
 $dbname = 'pz_bakery';
-
-function db_connect () {
 $conn = mysqli_connect($servername, $username, $password, $dbname);
-
 if (!$conn) {
-	
 	die("No so much Success");
 }
 return $conn;
-
 }
-
 function db_query(string $query) {
-
 	$conn = db_connect();
 	$result = $conn->query ($query);
 	$conn->close();
 	return $result;
 }
+$query = "SELECT * FROM `bakery_products`";
 
-/*$result = ($conn->query ('SHOW TABLES'));
+$result = db_query($query);
+
 foreach ($result as $key => $value) {
-	print_r($key);
 	print_r($value);
 }
 
-mysqli_close($conn);*/
