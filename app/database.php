33a0c101de4f558ffe74af418d63c0f72b39058f
Paulius Žemelis <paulius.zemelis@gmail.com>
2017-11-30 20:33:11 +0200

    Not so much

--- app/database.php
+++ app/database.php
@@ -10,7 +10,7 @@ $dbname = 'pz_bakery';
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
 if (!$conn) {
-	die("No so much Success");
+	die("Not so much Success");
 }
 $conn->set_charset("utf8mb4");
 return $conn;
